2c2839386f4a96c47b3979804ecc94f847818cf0
ethanloewen <ethanloewen@gmail.com>
2022-04-13 20:14:00 -0700

    Fix bug where payment is avaliable with empty cart

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,8 @@
 class Admin::CategoriesController < ApplicationController
+
+  # authentication
+  http_basic_authenticate_with :name => ENV['AUTH_USERNAME'], :password => ENV['AUTH_PASSWORD']
+
   def index
     @categories = Category.order(id: :desc).all
   end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+
+  # authentication
+  http_basic_authenticate_with :name => ENV['AUTH_USERNAME'], :password => ENV['AUTH_PASSWORD']
+
   def show
     @count = Product.count
     @categories = Category.count
